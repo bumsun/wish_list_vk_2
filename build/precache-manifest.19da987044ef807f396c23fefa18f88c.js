09d971653e163915469903e2dc54789283732ba5
Владимир <vladimir@MacBook-Pro-Vladimir-2.local>
2019-12-10 16:40:34 +0300

    message

---
+++ build/precache-manifest.19da987044ef807f396c23fefa18f88c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3e82a116f0428e8fb16f07b41a9ad48c",
+    "url": "/wish_list_vk_2/index.html"
+  },
+  {
+    "revision": "ec9a2eb8307ab19eaa26",
+    "url": "/wish_list_vk_2/static/css/2.2cfa1b23.chunk.css"
+  },
+  {
+    "revision": "ec9a2eb8307ab19eaa26",
+    "url": "/wish_list_vk_2/static/js/2.d866d0d5.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/wish_list_vk_2/static/js/2.d866d0d5.chunk.js.LICENSE"
+  },
+  {
+    "revision": "4f7a34ff3ee54bcf97ca",
+    "url": "/wish_list_vk_2/static/js/main.a99a7be4.chunk.js"
+  },
+  {
+    "revision": "1690515e8aa314491630",
+    "url": "/wish_list_vk_2/static/js/runtime-main.959e5bb1.js"
+  }
+]);
