5d846982a962c161eaf0a09ac052b7f819196bbe
Владимир <vladimir@MacBook-Pro-Vladimir-2.local>
2019-12-10 16:22:00 +0300

    message

--- src/App.js
+++ src/App.js
@@ -1,44 +1,22 @@
-import React, { useState, useEffect } from 'react';
-import connect from '@vkontakte/vk-connect';
-import View from '@vkontakte/vkui/dist/components/View/View';
-import ScreenSpinner from '@vkontakte/vkui/dist/components/ScreenSpinner/ScreenSpinner';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { View, Panel, PanelHeader, Group, List, Cell } from '@vkontakte/vkui';
 import '@vkontakte/vkui/dist/vkui.css';
 
-import Home from './panels/Home';
-import Persik from './panels/Persik';
-
-const App = () => {
-	const [activePanel, setActivePanel] = useState('home');
-	const [fetchedUser, setUser] = useState(null);
-	const [popout, setPopout] = useState(<ScreenSpinner size='large' />);
-
-	useEffect(() => {
-		connect.subscribe(({ detail: { type, data }}) => {
-			if (type === 'VKWebAppUpdateConfig') {
-				const schemeAttribute = document.createAttribute('scheme');
-				schemeAttribute.value = data.scheme ? data.scheme : 'client_light';
-				document.body.attributes.setNamedItem(schemeAttribute);
-			}
-		});
-		async function fetchData() {
-			const user = await connect.sendPromise('VKWebAppGetUserInfo');
-			setUser(user);
-			setPopout(null);
-		}
-		fetchData();
-	}, []);
-
-	const go = e => {
-		setActivePanel(e.currentTarget.dataset.to);
-	};
-
-	return (
-		<View activePanel={activePanel} popout={popout}>
-			<Home id='home' fetchedUser={fetchedUser} go={go} />
-			<Persik id='persik' go={go} />
-		</View>
-	);
+function App () {
+  return (
+    <View activePanel="main">
+      <Panel id="main">
+        <PanelHeader>VKUI</PanelHeader>
+        <Group title="Items">
+          <List>
+            <Cell>Hello</Cell>
+            <Cell>World</Cell>
+          </List>
+        </Group>
+      </Panel>
+    </View>
+  );
 }
 
-export default App;
-
+ReactDOM.render(<App />, document.getElementById('root'));
