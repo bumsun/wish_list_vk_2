24b50472fc1e691bb3822e423aa00beaefcdab71
Владимир <vladimir@MacBook-Pro-Vladimir-2.local>
2019-12-13 18:24:08 +0300

    message

--- src/App.js
+++ src/App.js
@@ -4,19 +4,20 @@ import { View, Panel, PanelHeader, Group, List, Cell } from '@vkontakte/vkui';
 import '@vkontakte/vkui/dist/vkui.css';
 
 function App () {
-  return (
-    <View activePanel="main">
-      <Panel id="main">
-        <PanelHeader>VKUI</PanelHeader>
-        <Group title="Items">
-          <List>
-            <Cell>Hello</Cell>
-            <Cell>World</Cell>
-          </List>
-        </Group>
-      </Panel>
-    </View>
-  );
+  return new Epic();
+  // return (
+  //   <View activePanel="main">
+  //     <Panel id="main">
+  //       <PanelHeader>VKUI</PanelHeader>
+  //       <Group title="Items">
+  //         <List>
+  //           <Cell>Hello</Cell>
+  //           <Cell>World</Cell>
+  //         </List>
+  //       </Group>
+  //     </Panel>
+  //   </View>
+  // );
 }
 
 ReactDOM.render(<App />, document.getElementById('root'));
