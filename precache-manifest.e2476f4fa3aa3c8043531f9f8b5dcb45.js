934ae149a0dde16941f9c7e14b91df726f8d9076
Владимир <vladimir@MacBook-Pro-Vladimir-2.local>
2019-12-13 20:43:49 +0300

    Updates

--- precache-manifest.e2476f4fa3aa3c8043531f9f8b5dcb45.js
+++ precache-manifest.e2476f4fa3aa3c8043531f9f8b5dcb45.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "4975b0c2c7cc46819973af66a9524bbf",
+    "revision": "af88770c17f93943515e1bff51b2ab94",
     "url": "/wish_list_vk_2/index.html"
   },
   {
-    "revision": "7f25a7203aaefaab8e44",
+    "revision": "92149155fa2c7a18e28b",
     "url": "/wish_list_vk_2/static/css/2.2cfa1b23.chunk.css"
   },
   {
-    "revision": "7f25a7203aaefaab8e44",
-    "url": "/wish_list_vk_2/static/js/2.37b8b9e2.chunk.js"
+    "revision": "92149155fa2c7a18e28b",
+    "url": "/wish_list_vk_2/static/js/2.047de336.chunk.js"
   },
   {
     "revision": "d705cb622423d72c5defbf368ca70dcc",
-    "url": "/wish_list_vk_2/static/js/2.37b8b9e2.chunk.js.LICENSE"
+    "url": "/wish_list_vk_2/static/js/2.047de336.chunk.js.LICENSE"
   },
   {
-    "revision": "e79b593c06773c62d2cd",
-    "url": "/wish_list_vk_2/static/js/main.38f49930.chunk.js"
+    "revision": "5230ae4bb2a500379730",
+    "url": "/wish_list_vk_2/static/js/main.20c6389c.chunk.js"
   },
   {
     "revision": "1690515e8aa314491630",
