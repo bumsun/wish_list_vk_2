121be12c268494aa37a71b66185832936968d54b
Владимир <vladimir@MacBook-Pro-Vladimir-2.local>
2019-12-22 04:37:13 +0300

    message

--- build/precache-manifest.4beab2762743219832485e14883cc8a7.js
+++ build/precache-manifest.4beab2762743219832485e14883cc8a7.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "99c50bbec83d25d58e75cb2594634229",
+    "revision": "6346f329c03dfcbd2e53816a48ff8c3b",
     "url": "/wish_list_vk_2/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/wish_list_vk_2/static/css/2.2cfa1b23.chunk.css"
   },
   {
-    "revision": "be7eae74c9a916c661f1",
+    "revision": "1f6a7ee411a001148e9f",
     "url": "/wish_list_vk_2/static/css/main.9164abe1.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/wish_list_vk_2/static/js/2.969cdad0.chunk.js"
   },
   {
-    "revision": "be7eae74c9a916c661f1",
-    "url": "/wish_list_vk_2/static/js/main.4e9e9c5b.chunk.js"
+    "revision": "1f6a7ee411a001148e9f",
+    "url": "/wish_list_vk_2/static/js/main.bcc7706e.chunk.js"
   },
   {
     "revision": "55b4db68469f309c3466",
