35eaae1e9cf7ec1277f978d4e3a120e0f59a3ee8
bumsun <bumsun@yandex.ru>
2020-03-12 01:18:09 +0300

    Updates

--- precache-manifest.2e8bd46622a523ae018b99c4206088a0.js
+++ precache-manifest.2e8bd46622a523ae018b99c4206088a0.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "a61a1b10354f81065f4c22dc254ad502",
+    "revision": "172d2e12e37420d26f66c38751f585fc",
     "url": "/wish_list_vk_2/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/wish_list_vk_2/static/css/2.2cfa1b23.chunk.css"
   },
   {
-    "revision": "ec57556281585eb731cd",
+    "revision": "2e0522419be8ac87a330",
     "url": "/wish_list_vk_2/static/css/main.9164abe1.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/wish_list_vk_2/static/js/2.969cdad0.chunk.js"
   },
   {
-    "revision": "ec57556281585eb731cd",
-    "url": "/wish_list_vk_2/static/js/main.ec68fc66.chunk.js"
+    "revision": "2e0522419be8ac87a330",
+    "url": "/wish_list_vk_2/static/js/main.58d86363.chunk.js"
   },
   {
     "revision": "55b4db68469f309c3466",
