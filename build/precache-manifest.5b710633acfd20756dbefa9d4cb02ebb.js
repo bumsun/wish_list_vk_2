4f84929349a73b49f14dc382503320df4bf942d7
bumsun <bumsun@yandex.ru>
2020-03-16 18:07:35 +0300

    message

---
+++ build/precache-manifest.5b710633acfd20756dbefa9d4cb02ebb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ea0eecd8f226f6b9134f27a36f65161",
+    "url": "/wish_list_vk_2/index.html"
+  },
+  {
+    "revision": "b924beb804043e57fd98",
+    "url": "/wish_list_vk_2/static/css/2.733ba2d0.chunk.css"
+  },
+  {
+    "revision": "729ed581374cff7c894a",
+    "url": "/wish_list_vk_2/static/css/main.9164abe1.chunk.css"
+  },
+  {
+    "revision": "b924beb804043e57fd98",
+    "url": "/wish_list_vk_2/static/js/2.ccde9b19.chunk.js"
+  },
+  {
+    "revision": "729ed581374cff7c894a",
+    "url": "/wish_list_vk_2/static/js/main.e30bb8b9.chunk.js"
+  },
+  {
+    "revision": "55b4db68469f309c3466",
+    "url": "/wish_list_vk_2/static/js/runtime-main.b3ea2764.js"
+  }
+]);
