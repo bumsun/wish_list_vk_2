c8517352b44aa511089f8d8a4b9cab3f5d376148
bumsun <bumsun@yandex.ru>
2020-03-24 17:25:19 +0300

    Updates

--- precache-manifest.27fb974ffd3c6f609992452d9e60c8f9.js
+++ precache-manifest.27fb974ffd3c6f609992452d9e60c8f9.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "176566abda34c97da8d82ae2835b56f7",
+    "revision": "d7e443dd35ed72568099d02208c9db55",
     "url": "./index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "./static/css/2.733ba2d0.chunk.css"
   },
   {
-    "revision": "cea7d33275872afc158c",
+    "revision": "44d66617eeb926edb589",
     "url": "./static/css/main.9164abe1.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "./static/js/2.43cc991d.chunk.js"
   },
   {
-    "revision": "cea7d33275872afc158c",
-    "url": "./static/js/main.e4a0a6e6.chunk.js"
+    "revision": "44d66617eeb926edb589",
+    "url": "./static/js/main.4247657c.chunk.js"
   },
   {
     "revision": "2ae5038bb09b04b096ca",
